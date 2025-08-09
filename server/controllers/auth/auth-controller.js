3d0d466e4c7d87e836b46134f4f71a6e6200993b
Seydur <seydur.rahman82@gmail.com>
2025-08-09 12:24:15 +0600

    cookie changes

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -68,7 +68,7 @@ const loginUser = async (req, res) => {
       "CLIENT_SECRET_KEY",
       { expiresIn: "60m" }
     );
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully",
       email: checkUser.email,
